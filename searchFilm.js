f41907384f90e5c1b50fba5a7ad48d7e81ee7eb8
onat68 <onat.rigault@gmail.com>
2023-08-01 15:34:00 +0200

    searchFilm mis dans une fonction

--- searchFilm.js
+++ searchFilm.js
@@ -1,17 +1,21 @@
 const fetch = require('node-fetch');
 
-toSearch = "la soupe aux choux"
+function searchFilm(toSearch){
 
-const url = `https://api.themoviedb.org/3/search/movie?query=${escape(toSearch)}`;
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5M2ViMWFhM2EzNDZkNTg5MWFkZDFjMWQ4MzM2ZGQ2NyIsInN1YiI6IjY0YzkwNDhiODlmNzQ5MDBhZTBiZmI5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WY3acLejoDB0otuZHhtAFelDy8ONHz9zJs_3pr1DHSk'
-  }
-};
 
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => {})
-  .catch(err => console.error('error:' + err));
+    const url = `https://api.themoviedb.org/3/search/movie?query=${escape(toSearch)}`;
+    const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5M2ViMWFhM2EzNDZkNTg5MWFkZDFjMWQ4MzM2ZGQ2NyIsInN1YiI6IjY0YzkwNDhiODlmNzQ5MDBhZTBiZmI5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WY3acLejoDB0otuZHhtAFelDy8ONHz9zJs_3pr1DHSk'
+    }
+    };
+
+    fetch(url, options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error('error:' + err));
+}
+
+searchFilm("entretien avec un vampire")
