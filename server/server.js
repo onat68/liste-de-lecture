e5fc5219811b8c276ae6520448d00242361b6bbb
LouisCoutel <louiscoutel75@gmail.com>
2024-01-14 21:24:43 +0100

    Set port listening

--- server/server.js
+++ server/server.js
@@ -40,8 +40,8 @@ const server = http.createServer(app)
 server.on('error', errorHandler)
 server.on('listening', () => {
     const address = server.address() // ??
-    const bind = typeof address === 'string' ? 'pipe' + address : 'port' + port
-    console.log('Listening on ' + bind)
-})
+    const bind = typeof address === 'string' ? 'pipe' + address : 'port' + port;
+    console.log('Listening on ' + bind);
+  });
 
-server.listen(port)
+  server.listen(port, '0.0.0.0');
