111ed44ea75f4a2470b64e654d952459c0c83e12
LouisCoutel <louiscoutel75@gmail.com>
2024-01-26 14:57:37 +0100

    Mergin List

--- VueList/src/stores/useSearchResultStore.js
+++ VueList/src/stores/useSearchResultStore.js
@@ -1,6 +1,6 @@
 /* eslint-disable eqeqeq */
 import { defineStore } from "pinia"
-import { types } from "../types"
+import { types } from "../itemTypes"
 
 export const useSearchResults = defineStore("useSearchResults", {
     state: () => ({
@@ -16,16 +16,31 @@ export const useSearchResults = defineStore("useSearchResults", {
         unpickedTypes: (state) => {
             return types.filter((obj) => obj != state.pickedType)
         },
+
+        all: (state) => {
+            return state.albums.concat(state.books, state.movies)
+        },
+
+        filtered(state) {
+            if (state.pickedType.name == "All") {
+                return this.all
+            } else {
+                return state[state.pickedType.name.toLowerCase() + "s"]
+            }
+        },
     },
 
     actions: {
         async find() {
             this.router.replace({ name: "load" })
             this.endSearch()
+
             const res = await fetch(`ext/find/${this.pickedType.name}/${this.query}`)
+
             this.router.replace({ name: "search" })
+
             const data = await res.json()
-            console.log(await data)
+
             this.albums = await data.albums
             this.movies = await data.movies
             this.books = await data.books
