2ebc54696a7d25f173d5c5efb47cad28c6fb6f0a
LouisCoutel <louiscoutel75@gmail.com>
2024-01-28 15:40:11 +0100

    Push portfolio-specific branch to List repo

--- server/routes/external.js
+++
@@ -1,6 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const externalCtrl = require("../controllers/external")
-
-router.get("/find/:type/:query", externalCtrl.find)
-module.exports = router
