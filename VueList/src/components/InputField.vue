550c33e738b82bc92826eb47e14c5832f4cf1ae4
LouisCoutel <louiscoutel75@gmail.com>
2024-02-28 09:23:08 +0100

    Fix: change server auth route url

--- VueList/src/components/InputField.vue
+++ VueList/src/components/InputField.vue
@@ -1,16 +1,19 @@
 <script setup>
-
+import { defineModel } from 'vue';
 const props = defineProps({
     label: String,
     name: String,
     required: Boolean,
     type: String
 })
+
+const model = defineModel()
+
 </script>
 
 <template>
     <label class="flex flex-col w-full p-1 gap-2 whitespace-nowrap overflow-hidden box-border items-center"
         :for="props.name">{{ label }}
-        <input class="flex grow box-border px-2 rounded-sm bg-slate-200 py-1" :type="props.type" :id="props.name"
-            :name="props.name" :required="props.required" /></label>
+        <input v-model="model" class="flex grow box-border px-2 rounded-sm bg-slate-200 py-1" :type="props.type"
+            :id="props.name" :name="props.name" :required="props.required" /></label>
 </template>
